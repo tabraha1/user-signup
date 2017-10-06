3c5b3f934c15cc1a6184918232e03c4d4fa7809b
Tem Abraha <temabraha1@gmail.com>
2017-10-05 23:05:51 -0500

    Changed login.html and validation errors in main.py

--- main.py
+++ main.py
@@ -1,26 +1,42 @@
 from flask import Flask, render_template, redirect, url_for, request
-import re
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+def valid_login(name):
+    if (name != "") and len(name) > 3 and len(name) < 20:
+        return True
+    else:
+        return False
+
+def valid_password(password):
+    if password != "" and len(password) > 3 and len(password) < 20:
+        return True
+    else:
+        return False
+
+def is_valid_email(email):
+    if "@" in email and "." in email:
+        return True
+    else:
+        return False
+
 @app.route('/', methods=['GET', 'POST'])
 def login():
-    error = None
+    username_error = ""
+    password_error = ""
+    verify_error = ""
+    email_error = ""
     if request.method == 'POST':
-        if request.form['username'] == '' or request.form['password'] == '':
-            error = 'Invalid Credentials. Please try again.'
-        elif len(request.form['username']) < 3 or len(request.form['password']) < 3:
-            error = 'Either username or password is too short.'
-        elif len(request.form['username']) > 20 or len(request.form['password']) > 20:
-            error = 'Either username or password is too short.'
-        elif request.form['password'] != request.form['verify']:
-            error = 'Password does not match.'
-        elif re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", request.form['email']) == None:
-            error = 'Not a valid email.'
-        else:
-            return redirect(url_for('secret'))
-    return render_template('login.html', error=error)
+        if not valid_login(request.form['username']): 
+            username_error='Invalid username'
+        if not valid_password(request.form['password']):
+            password_error='Invalid password'
+        if not request.form['password'] == request.form['verify']:
+            verify_error='Passwords do no match'
+        if not is_valid_email(request.form['email']):
+            email_error='Email invalid'
+    return render_template('login.html', username_error=username_error, password_error=password_error, verify_error=verify_error,email_error=email_error)
 
 if __name__ == '__main__':
     app.run()

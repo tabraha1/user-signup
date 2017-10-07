b3040f77ae5fbc246634ae4734875cf1f60e3f52
Tem Abraha <temabraha1@gmail.com>
2017-10-06 20:30:31 -0500

    Finalized project

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def valid_login(name):
         return False
 
 def valid_password(password):
-    if password != "" and len(password) > 3 and len(password) < 20:
+    if (password != "") and len(password) > 3 and len(password) < 20:
         return True
     else:
         return False
@@ -33,10 +33,19 @@ def login():
         if not valid_password(request.form['password']):
             password_error='Invalid password'
         if not request.form['password'] == request.form['verify']:
-            verify_error='Passwords do no match'
+            verify_error='Passwords do not match'
         if not is_valid_email(request.form['email']):
             email_error='Email invalid'
+        else:
+            if valid_login(request.form['username']) and valid_password(request.form['password']) and request.form['password'] == request.form['verify'] and is_valid_email(request.form['email']):
+                name = request.form['username']
+                return redirect('/welcome?name={0}'.format(name))
     return render_template('login.html', username_error=username_error, password_error=password_error, verify_error=verify_error,email_error=email_error)
+
+@app.route('/welcome')
+def welcome():
+    name = request.args.get('name')
+    return '<h1>Welcome {0} to the homepage!</h1>'.format(name)
 
 if __name__ == '__main__':
     app.run()
